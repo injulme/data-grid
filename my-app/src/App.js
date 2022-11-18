6780b3638aae074c1a3c3cfc2fac5d2b46683625
injulme <injulme0309@naver.com>
2022-11-19 00:01:09 +0900

    sorting 작업중

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,69 @@
+import { useMemo, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(initialData);
+  const [isAsc, setIsAsc] = useState(false);
+  const onSorting = (field) => {
+    const clone = [...data];
+    const sorting = clone.sort((a, b) =>
+      isAsc ? a[field] - b[field] : b[field] - a[field]
+    );
+
+    setData(sorting);
+    setIsAsc(!isAsc);
+  };
+
+  const memoData = useMemo(() => {
+    return data.map((v, i) => {
+      return (
+        <tr key={i}>
+          <td>{v.name}</td>
+          <td>{v.age}</td>
+          <td>{v.gender === "M" ? "남" : "여"}</td>
+        </tr>
+      );
+    });
+  }, [data]);
+
   return (
     <table>
       <thead>
         <tr>
-          <th>이름</th>
-          <th>나이</th>
-          <th>성별</th>
+          {columns.map((v, i) => {
+            return (
+              <th key={i} onClick={() => onSorting(v.field)}>
+                {v.label} {isAsc ? "^" : "v"}
+              </th>
+            );
+          })}
         </tr>
       </thead>
-      <tbody>
-        {data.map((v, i) => {
-          return (
-            <tr key={i}>
-              <td>{v.name}</td>
-              <td>{v.age}</td>
-              <td>{v.gender === "M" ? "남" : "여"}</td>
-            </tr>
-          );
-        })}
-      </tbody>
+      <tbody>{memoData}</tbody>
     </table>
   );
 }
 
-const data = [
+const columns = [
+  {
+    field: "name",
+    label: "이름",
+  },
+  {
+    field: "age",
+    label: "나이",
+  },
+  {
+    field: "gender",
+    label: "성별",
+  },
+];
+const initialData = [
+  {
+    name: "김미정",
+    age: 30,
+    gender: "W",
+  },
   {
     name: "정효진",
     age: 25,
@@ -40,6 +78,11 @@ const data = [
     name: "김태열",
     age: 27,
     gender: "M",
+  },
+  {
+    name: "지수림",
+    age: 25,
+    gender: "W",
   },
   {
     name: "홍성재",
